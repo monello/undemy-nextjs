ff850981c2a91524786b965f2f53ae0c5ea822c4
Morne <louw.morne@gmail.com>
2024-03-14 20:44:14 +0000

    set-up for new course project - main project - foodies app

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Link from 'next/link';
-import Header from '@/components/Header';
-
 export default function Home() {
     return (
         <main>
-            <Header />
-            <p>🔥 Let&apos;s get started! 🔥</p>
-            <p>
-                <Link href="/about">About Us</Link>
-            </p>
+            <h1 style={{ color: 'white', textAlign: 'center' }}>
+                Time to get started!
+            </h1>
         </main>
     );
 }
