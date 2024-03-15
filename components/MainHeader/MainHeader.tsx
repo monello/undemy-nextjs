2e07ad2e23bea9e7f54f1e2ab47d5dce6c3dba97
Morne <louw.morne@gmail.com>
2024-03-15 17:09:48 +0000

    prepare the slideshow component

--- components/MainHeader/MainHeader.tsx
+++ components/MainHeader/MainHeader.tsx
@@ -1,20 +1,20 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import logoImg from '@/assets/logo.png';
-import classes from './mainHeader.module.css';
 import MainHeaderBackground from '@/components/MainHeaderBackground/MainHeaderBackground';
+import logoImg from '@/assets/logo.png';
+import styles from './mainHeader.module.css';
 
 export default function MainHeader() {
     return (
         <>
             <MainHeaderBackground />
-            <header className={classes.header}>
-                <Link className={classes.logo} href="/">
+            <header className={styles.header}>
+                <Link className={styles.logo} href="/">
                     <Image src={logoImg} alt="A plate of food" priority />
                     NextLevel Food
                 </Link>
 
-                <nav className={classes.nav}>
+                <nav className={styles.nav}>
                     <ul>
                         <li>
                             <Link href="/meals">Browse Meals</Link>
