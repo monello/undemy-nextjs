a9d4694e7505a5bad575856b271e74f6ccdcadf0
Morne <louw.morne@gmail.com>
2024-03-15 17:32:35 +0000

    add community landing page content

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,16 +1,44 @@
-import Link from 'next/link';
+import Image from 'next/image';
+
+import mealIcon from '@/assets/icons/meal.png';
+import communityIcon from '@/assets/icons/community.png';
+import eventsIcon from '@/assets/icons/events.png';
+import styles from './page.module.css';
 
 export default function CommunityPage() {
     return (
-        <main>
-            <h1>Community</h1>
-            <nav>
-                <ul>
+        <>
+            <header className={styles.header}>
+                <h1>
+                    One shared passion:{' '}
+                    <span className={styles.highlight}>Food</span>
+                </h1>
+                <p>Join our community and share your favorite recipes!</p>
+            </header>
+            <main className={styles.main}>
+                <h2>Community Perks</h2>
+
+                <ul className={styles.perks}>
                     <li>
-                        <Link href="/">Home</Link>
+                        <Image src={mealIcon} alt="A delicious meal" />
+                        <p>Share & discover recipes</p>
+                    </li>
+                    <li>
+                        <Image
+                            src={communityIcon}
+                            alt="A crowd of people, cooking"
+                        />
+                        <p>Find new friends & like-minded people</p>
+                    </li>
+                    <li>
+                        <Image
+                            src={eventsIcon}
+                            alt="A crowd of people at a cooking event"
+                        />
+                        <p>Participate in exclusive events</p>
                     </li>
                 </ul>
-            </nav>
-        </main>
+            </main>
+        </>
     );
 }
