bda06036433992f2b500cefce0b8c77ba1b38388
Morne <louw.morne@gmail.com>
2024-03-15 16:14:16 +0000

    styling using css modules

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,16 +1,16 @@
 import Link from 'next/link';
-
 import logoImg from '@/assets/logo.png';
+import classes from './mainHeader.module.css';
 
 export default function MainHeader() {
     return (
-        <header>
-            <Link href="/">
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
                 <img src={logoImg.src} alt="A plate of food" />
                 NextLevel Food
             </Link>
 
-            <nav>
+            <nav className={classes.nav}>
                 <ul>
                     <li>
                         <Link href="/meals">Browse Meals</Link>
