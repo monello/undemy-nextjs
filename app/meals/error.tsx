66fce19bd355f8fdd528ba2921311d3acb0d290c
Morne <louw.morne@gmail.com>
2024-03-16 19:57:03 +0000

    handling errors in a more typescript way

--- app/meals/error.tsx
+++ app/meals/error.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-export default function MealsError() {
+interface CustomError {
+    message: string;
+    // Add any additional properties you need
+}
+
+interface MealsErrorProps {
+    error: CustomError;
+}
+
+export default function MealsError({ error }: MealsErrorProps) {
     return (
         <main className="error">
             <h1>An error occured!</h1>
             <p>Failed to load meals data. Please try again later</p>
+            <p>{error.message}</p>
         </main>
     );
 }
