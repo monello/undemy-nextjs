a3c8121a7719ca6c3ae61b804a2e1bffb59e9a56
Morne <louw.morne@gmail.com>
2024-03-16 19:52:35 +0000

    demonstrating an error.tsx component

--- lib/meals.ts
+++ lib/meals.ts
@@ -5,6 +5,7 @@ const db = sql('meals.db');
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
+  throw new Error('Loading meals failed');
   try {
     const stmt: Statement<Meal[]> = db.prepare('SELECT * FROM meals');
     const meals: Meal[] = stmt.all() as Meal[]; // Type assertion
