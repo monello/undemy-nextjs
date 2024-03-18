dbf1af09b890f547ade51f5739c05e0fd6f8d209
Morne <louw.morne@gmail.com>
2024-03-18 01:10:57 +0000

    basic server-side  data-validation - NB not ideal approach, but works for demo purposes

--- lib/actions.tsx
+++ lib/actions.tsx
@@ -4,6 +4,10 @@ import { MealFormData } from '@/components/Meals/types';
 import { saveMeal } from './meals';
 import { redirect } from 'next/navigation';
 
+function isInvalidText(text: string) {
+    return !text || text.trim() === '';
+}
+
 export async function shareMeal(formData: FormData) {
     const meal: MealFormData = {
         title: formData.get('title') as string,
@@ -13,6 +17,19 @@ export async function shareMeal(formData: FormData) {
         creator: formData.get('name') as string,
         creator_email: formData.get('email') as string,
     };
+
+    if (
+        isInvalidText(meal.title) ||
+        isInvalidText(meal.summary) ||
+        isInvalidText(meal.instructions) ||
+        isInvalidText(meal.creator) ||
+        isInvalidText(meal.creator_email) ||
+        !meal.creator_email.includes('@') ||
+        !meal.image ||
+        meal.image.size === 0
+    ) {
+        throw new Error('Invalid input');
+    }
 
     await saveMeal(meal);
     redirect('/meals');
